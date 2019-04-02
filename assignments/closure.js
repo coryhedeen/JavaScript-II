83c72450156d7ce3cf635077601ec0f25a92619c
Cory Hedeen <hedeencory@gmail.com>
2019-04-02 15:20:02 -0400

    finished challenge 1

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,9 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+const thing = 'closure';
+function (){
+  return thing;
+}
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
